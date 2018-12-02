445c635e7559dca5a6e5df9e99bb2189fe170fd8
Seeker Torchbugs <storchbu@e3r2p15.21-school.ru>
2018-12-02 19:09:04 +0300

    all

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,44 +6,23 @@
 /*   By: storchbu <storchbu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/02 16:50:37 by storchbu          #+#    #+#             */
-/*   Updated: 2018/12/02 17:23:17 by storchbu         ###   ########.fr       */
+/*   Updated: 2018/12/02 19:08:20 by storchbu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int		ft_memcmp(char *str1, char *str2, int n)
+int		ft_memcmp(const void *one, const char *two, size_t n)
 {
 	int res;
 
 	res = 0;
-	while (*str1 || *str2)
+	while (*one || *two)
 	{
-		if (*str1 > *str2)
+		if (*one > *two)
 			res++;
-		else if (*str1 < *str2)
+		else if (*one < *two)
 			res--;
-		str1++;
-		str2++;
+		one++;
+		two++;
 	}
 	return (res);
-}
-
-int main(void)
-{
-	char str1[15] = "abcdef";
-	char str2[15] = "abcdef";
-   	int ret;
-
-   ret = ft_memcmp(str1, str2, 5);
-
-   if(ret > 0) {
-      printf("str2 is less than str1");
-   } else if(ret < 0) {
-      printf("str1 is less than str2");
-   } else {
-      printf("str1 is equal to str2");
-   }
-   
-   return(0);
 }
