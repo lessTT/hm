df2ce35a962467940dae112fcf81f99b9d2450ea
Nikita Vasilyev <nikitavv@MacBook-NVV.local>
2018-12-16 23:38:31 +0300

    add

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: storchbu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/12 05:36:11 by storchbu          #+#    #+#             */
-/*   Updated: 2018/12/12 06:20:25 by storchbu         ###   ########.fr       */
+/*   Updated: 2018/12/16 20:48:15 by storchbu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,3 +22,32 @@ size_t	f_strlcat(char *dest, const char *src, size_t size)
 	total = ft_strlen(dest);
 	return (total);
 }
+
+
+// {
+// 	char		*d;
+// 	const char	*s;
+// 	size_t		n;
+// 	size_t		dlen;
+
+// 	d = dst;
+// 	s = src;
+// 	n = size;
+// 	while (n-- != 0 && *d != '\0')
+// 		d++;
+// 	dlen = d - dst;
+// 	n = size - dlen;
+// 	if (n == 0)
+// 		return (dlen + ft_strlen(s));
+// 	while (*s != '\0')
+// 	{
+// 		if (n != 1)
+// 		{
+// 			*d++ = *s;
+// 			n--;
+// 		}
+// 		s++;
+// 	}
+// 	*d = '\0';
+// 	return (dlen + (s - src));
+// }
