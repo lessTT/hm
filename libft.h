73dab9274cc14884a178172d77820a77fcbf0e6b
Nikita Vasilyev <nikitavv@MacBook-NVV.local>
2019-01-15 12:45:47 +0300

    sec

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: storchbu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 19:31:11 by pcollio-          #+#    #+#             */
-/*   Updated: 2018/12/12 05:12:21 by storchbu         ###   ########.fr       */
+/*   Updated: 2019/01/11 16:59:02 by storchbu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,25 +21,25 @@
 #include <stdio.h> //DELETE
 
 /* ***** Part 1 - Libc functions ***** */
-// int			ft_atoi(const char *str);
+int			ft_atoi(const char *str);
 void	*ft_bzero(void *s, size_t n);
-// int			ft_isalnum(int c);
-// int			ft_isalpha(int c);
-// int			ft_isascii(int c);
-// int			ft_isdigit(int c);
-// int			ft_isprint(int c);
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
 void		*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void	*ft_memchr (const void *str, int c, size_t n);
 int		ft_memcmp(const void *one, const void *two, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
-//*ft_memmove
+void	*ft_memmove(void *dest, const void *src, size_t nb);
 void	*ft_memset(void *a, int val, size_t num);
 char		*ft_strcat(char *dest, const char *src);
-//*ft_strchr
-// int			ft_strcmp(const char *str1, const char *str2);
-// char		*ft_strcpy(char *dest, const char *src);
+char	*ft_strchr(const char *str, int c);
+int			ft_strcmp(const char *str1, const char *str2);
+char		*ft_strcpy(char *dest, const char *src);
 // char		*ft_strdup(char *src);
-// size_t		ft_strlcat(char *dest, const char *src, size_t size);
+size_t		ft_strlcat(char *dest, const char *src, size_t size);
 size_t		ft_strlen(const char *str);
 // char		*ft_strncat(char *dest, char *src, size_t n);
 // int			ft_strncmp(const char *str1, const char *str2, size_t n);
@@ -50,30 +50,31 @@ size_t		ft_strlen(const char *str);
 // int			ft_toupper(int c);
 // int			ft_tolower(int c);
 /* ***** Part 2 - Libc functions ***** */
-//ft_memalloc
-//ft_memdel
-//ft_strnew
-//ft_strdel
-//ft_strclr
-//ft_striter
-//ft_striteri
-//ft_strmap
-//ft_strmapi
-//ft_strequ
-//ft_strnequ
-//ft_strsub
-//ft_strjoin
-//ft_strtrim
-//ft_strsplit
-//ft_itoa
-//ft_putchar
-//ft_putstr
-//ft_putendl
-//ft_putnbr;
-//ft_putchar_fd
-//ft_putstr_fd
-//ft_putendl_fd
-//ft_putnbr_fd
+void	*ft_memalloc(size_t size);
+void	ft_memdel(void **ap);
+char	*ft_strnew(size_t size);
+void	ft_striter(char *s, void (*f)(char *));
+void	ft_strclr(char *s);
+
+// ft_striter
+// ft_striteri
+// ft_strmap
+// ft_strmapi
+// ft_strequ
+// ft_strnequ
+// ft_strsub
+// ft_strjoin
+// ft_strtrim
+// ft_strsplit
+// ft_itoa
+// ft_putchar
+// ft_putstr
+// ft_putendl
+// ft_putnbr;
+// ft_putchar_fd
+// ft_putstr_fd
+// ft_putendl_fd
+// ft_putnbr_fd
 
 /* ***** Part 3 - Bonus part ***** */
 //ft_lstnew
